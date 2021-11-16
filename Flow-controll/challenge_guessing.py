b79dc13539a3f7328ac02a3a9b6738805937794c
dovankha <dovankha.tlvn@gmail.com>
2021-11-16 09:44:32 +0700

    Update Flow controll

--- Flow-controll/challenge_guessing.py
+++ Flow-controll/challenge_guessing.py
@@ -1,1 +1,22 @@
 import random
+import os
+highest = 10
+answer = random.randint(1, highest)
+# print(answer)  # TODO: remove after testing
+guess = 0
+print("Please enter number between 1 and {}.".format(highest))
+while guess != answer:
+    guess = int(input("Guess = "))
+    if guess == 0:
+        print("Thank you!")
+        os.system("pause")
+        break
+    elif guess == answer:
+        print("You Win! Contact to KhaDepTrai recive money...")
+        os.system("pause")
+        break
+    else:
+        if guess < answer:
+            print("Please guess higher.")
+        else:
+            print("Please guess lower.")
