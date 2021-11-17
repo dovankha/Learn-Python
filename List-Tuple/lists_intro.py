91d6518228ee74b2a6d5193096929a3f1f3723cf
dovankha <dovankha.tlvn@gmail.com>
2021-11-17 21:39:05 +0700

    Update List

---
+++ List-Tuple/lists_intro.py
@@ -0,0 +1,5 @@
+computer_parts = ['computer', 'monitor', 'keyboard', 'mouse', 'mouse mat']
+for part in computer_parts:
+    print(part)
+print(computer_parts[3])
+print(computer_parts[0:3:1])  # chỉ in tới 2 (0, 1 , 2). Bước nhảy là 1.
