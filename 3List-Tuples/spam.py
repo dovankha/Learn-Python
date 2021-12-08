792b7eadef40342c8ffa765bc1a0ee19ac817a5a
dovankha <dovankha.tlvn@gmail.com>
2021-12-08 11:50:29 +0700

    Upate nested list

---
+++ 3List-Tuples/spam.py
@@ -0,0 +1,31 @@
+menu = [
+    ["egg", "bacon"],
+    ["egg", "sausage", "bacon"],
+    ["egg", "spam"],
+    ["spam", "bacon", "sausage", "spam"]
+]
+
+# nested list
+for meal in menu:
+    if "spam" in meal:
+        print(meal)
+
+        for item in meal:
+            print(item)
+    else:
+        print("{} has a spam score of {}".format(meal, meal.count("spam")))
+
+# cách 1: xoá item "spam" ra khỏi list
+for meal in menu:
+    for index in range(len(meal) - 1, -1, -1):
+        if meal[index] == "spam":
+            del meal[index]
+
+    print(meal)
+
+# cách 2: in item không phải "spam"
+for meal in menu:
+    for item in meal:
+        if item == "spam":
+            print(item)
+    print()
