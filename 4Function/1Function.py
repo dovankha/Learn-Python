ca9475dfe4efcd8dcd585cb2b322893639cba4a5
dovankha <dovankha.tlvn@gmail.com>
2021-12-29 10:22:23 +0700

    Update Function

--- 4Function/1Function.py
+++ 4Function/1Function.py
@@ -15,16 +15,27 @@ for val in range(1, 11):
     print("2 * ", val, " = ", two_times)
 
 
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+    # return string[::-1].casefold() == string.casefold()
+    return palindrome_sentence(string)
+
+
 word = input("Please enter a word to check: ")
 if is_palindrome(word):
     print("{} is a palindrome.".format(word))
 else:
     print("{} is not a palindrome.".format(word))
 
+str_test = "radar"
 
-str_test = "CONGACON"
+print("Palindrome_sentence: ", palindrome_sentence(str_test))
+
 print(str_test.islower())
-print(str_test.capitalize()) # in hoa kí tự đầu tiên: Congacon
+print(str_test.capitalize())  # in hoa kí tự đầu tiên: Congacon
 print(str_test.casefold())
 print(str_test.lower())
 print(str_test.upper())
